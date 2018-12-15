b4175d3bae26cda58eab5f3917006158b9d73a72
Sachin Tewari <xachin@gmail.com>
2018-12-15 12:43:08 +0800

    Add controller support

--- app/src/main/java/com/stepango/okspam/MainActivity.kt
+++ app/src/main/java/com/stepango/okspam/MainActivity.kt
@@ -2,11 +2,63 @@ package com.stepango.okspam
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.InputDevice
+import android.view.KeyEvent
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : Activity() {
+
+    private val vm : MainViewModel = MainViewModelImpl()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViews()
     }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        var handled = false
+        if (event.source and InputDevice.SOURCE_GAMEPAD == InputDevice.SOURCE_GAMEPAD) {
+            if (event.repeatCount == 0) {
+                keyCode.takeIf { it.isFireKey() }?.run {
+
+                    handled = true
+                }
+            }
+            if (handled) {
+                return true
+            }
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun setupViews() {
+        // Keep listening to status of controllers
+        Observable.interval(0, 5, TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnNext { checkCurrentInputs() }
+            .subscribe()
+    }
+
+    private fun checkCurrentInputs() {
+        vm.controllerIds()
+            .map {
+                if (it.isNotEmpty()) {
+                    "Controller connected!" to R.color.green
+                } else {
+                    "Controller disconnected!" to R.color.red
+                }
+            }
+            .doOnSuccess {
+                controllerStatus.text = it.first
+                controllerStatus.setTextColor(resources.getColor(it.second))
+            }
+            .subscribe()
+    }
+
 }
--- app/src/main/java/com/stepango/okspam/MainViewModel.kt
+++ app/src/main/java/com/stepango/okspam/MainViewModel.kt
@@ -1,13 +1,47 @@
 package com.stepango.okspam
 
+import android.view.InputDevice
 import com.stepango.okspam.arch.ViewModel
-import io.reactivex.Observable
-import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.Single
 
 
-class MainViewModel : ViewModel {
+interface MainViewModel : ViewModel {
 
-    private val controllerIdSubject = BehaviorSubject.createDefault(emptyList<Int>())
+    fun controllerIds(): Single<List<Int>>
+}
 
-    fun controllerId(): Observable<List<Int>> = controllerIdSubject
+
+class MainViewModelImpl : MainViewModel {
+
+    init {
+        setup()
+    }
+
+    override fun controllerIds(): Single<List<Int>> = Single.create<List<Int>> { emitter ->
+        emitter.onSuccess(getGameControllerIds())
+    }
+
+    private fun setup() {
+
+    }
+
+    private fun getGameControllerIds(): List<Int> {
+        val gameControllerDeviceIds = mutableListOf<Int>()
+        val deviceIds = InputDevice.getDeviceIds()
+        deviceIds.forEach { deviceId ->
+            InputDevice.getDevice(deviceId).apply {
+
+                // Verify that the device has gamepad buttons, control sticks, or both.
+                if (sources and InputDevice.SOURCE_GAMEPAD == InputDevice.SOURCE_GAMEPAD
+                    || sources and InputDevice.SOURCE_JOYSTICK == InputDevice.SOURCE_JOYSTICK) {
+                    // This device is a game controller. Store its device ID.
+                    gameControllerDeviceIds
+                        .takeIf { !it.contains(deviceId) }
+                        ?.add(deviceId)
+                }
+            }
+        }
+        return gameControllerDeviceIds
+    }
+
 }
