c64d37deeed42fc9d0cbf371e2a9e44e8fd7264a
Sachin Tewari <xachin@gmail.com>
2018-12-15 17:37:55 +0800

    Set minSdk to 21

--- app/src/main/java/com/stepango/okspam/MainActivity.kt
+++ app/src/main/java/com/stepango/okspam/MainActivity.kt
@@ -3,23 +3,29 @@ package com.stepango.okspam
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
+
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (Settings.canDrawOverlays(this)) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Settings.canDrawOverlays(this)) {
 
-            // Launch service right away - the user has already previously granted permission
-            launchMainService()
+                // Launch service right away - the user has already previously granted permission
+                launchMainService()
+            } else {
+
+                // Check that the user has granted permission, and prompt them if not
+                checkDrawOverlayPermission()
+            }
         } else {
-
-            // Check that the user has granted permission, and prompt them if not
-            checkDrawOverlayPermission()
+            launchMainService()
         }
     }
 
@@ -38,13 +44,13 @@ class MainActivity : Activity() {
         finish()
     }
 
-    fun checkDrawOverlayPermission() {
+    private fun checkDrawOverlayPermission() {
 
         // Checks if app already has permission to draw overlays
-        if (!Settings.canDrawOverlays(this)) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
 
             // If not, form up an Intent to launch the permission request
-            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()))
+            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
 
             // Launch Intent, with the supplied request code
             startActivityForResult(intent, REQUEST_CODE)
@@ -57,7 +63,7 @@ class MainActivity : Activity() {
         if (requestCode == REQUEST_CODE) {
 
             // Double-check that the user granted it, and didn't just dismiss the request
-            if (Settings.canDrawOverlays(this)) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&  Settings.canDrawOverlays(this)) {
 
                 // Launch the service
                 launchMainService()
